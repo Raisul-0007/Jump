26b0fbd6b0c482b5425bcceea1f5b0d25acadfb0
Raisul-0007 <raisulislamriat007@gmail.com>
2026-07-13 11:22:01 +0600

    offer custom arrow

--- src/components/New.jsx
+++ src/components/New.jsx
@@ -6,7 +6,7 @@ const Slider = SliderModule.default;
 const New = () => {
   const settings = {
     infinite: true,
-    speed: 500,
+    speed: 1000,
     slidesToShow: 4,
     slidesToScroll: 2,
     waitForAnimate: false,
@@ -24,7 +24,7 @@ const New = () => {
             {info.slice(176, 190).map((item)=>(
               <div key={item.id} className=" px-3  ">
                 <div className="border rounded-4xl">
-                <div className="rounded-t-4xl p-5 bg-[#ffffff23]">
+                <div className="rounded-t-4xl p-5 bg-[#ffffff13]">
                   <img src={item.thumbnail} alt={item.id} />
                 </div>
                 <div className="bg-red-500 rounded-b-4xl px-5 pt-2 pb-4">
@@ -33,12 +33,12 @@ const New = () => {
                     <h2 className="text-white lg:text-2xl text-xl">{item.title.slice(0, 12)}</h2>
                   </div>
                   <div className="">
-                    <p className=" lg:text-[16px] text-[12px]">{(item.price-(item.price * item.discountPercentage) / 100).toFixed(2)}</p>
+                    <p className=" lg:text-[16px] text-[12px]">${(item.price-(item.price * item.discountPercentage) / 100).toFixed(2)}</p>
                   </div>
                 </div>
                 <div className="flex justify-between">
                     <p className="capitalize lg:text-sm text-[12px] text-[#d3d1d1]">{item.brand ? item.brand : "Local"}</p>
-                    <p className="lg:text-[12px]   text-[10px] text-black line-through">{item.price}</p>
+                    <p className="lg:text-[12px]   text-[10px] text-black line-through">${item.price}</p>
                 </div>
                 </div>
                 </div>
--- src/components/Offer.jsx
+++ src/components/Offer.jsx
@@ -1,13 +1,16 @@
-import React, { useContext } from 'react'
+import React, { useContext, useRef } from 'react'
 import Container from './Container';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
-import { Navigation } from "swiper/modules";
+import { Autoplay, Navigation } from "swiper/modules";
 import { Data } from './ApiData';
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
 
 const Offer = () => {
   let info = useContext(Data)
+         const prevRef = useRef(null)
+         const nextRef = useRef(null)
 
   return (
     <div className="text-white py-12.5">
@@ -15,29 +18,61 @@ const Offer = () => {
         <div className="">
           <h3 className="italic text-4xl text-center font-semibold">Offer</h3>
         </div>
-        <div className="py-20">
+        <div className="py-10">
+          <div className="flex justify-end gap-4 mb-5">
+            <button ref={prevRef} className="p-3 bg-[#ffffff13] rounded-lg text-red-500">
+              <FaArrowLeft />
+            </button>
+            <button ref={nextRef} className="p-3 bg-[#ffffff13] text-red-500 rounded-lg">
+              <FaArrowRight />
+            </button>
+          </div>
           <Swiper
         slidesPerView={5}
-        spaceBetween={30}
-        navigation
-        modules={[Navigation]}
+        spaceBetween={20}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2000,
+          disableOnInteraction: false,
+        }}
+        breakpoints={{
+          320: {
+            slidesPerView: 1,
+          },
+          576: {
+            slidesPerView: 2,
+          },
+          768: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 5,
+            spaceBetween: 50,
+          },
+        }}
+        navigation={{
+          prevEl: prevRef.current,
+          nextEl: nextRef.current,
+        }}
+        modules={[Navigation, Autoplay]}
         className="mySwiper"
       >
         {info.map((item)=>(
           <SwiperSlide>
-            <div className="bg-[#ffffff23] py-5 my-auto lg:min-h-70">
+            <div className="bg-[#ffffff13] py-5 my-auto lg:min-h-70">
               <img className="w-full" src={item.thumbnail} alt="" />
             </div>
-            <div className="">
+            <div className="py-4  ">
               <div className="flex justify-between items-center">
                 <h4 className="lg:text-2xl text-xl">{item.title.slice(0, 12)}</h4>
-                <p className="lg:text-[16px] text-[12px] text-red-500">{(item.price-(item.price * item.discountPercentage) / 100).toFixed(2)}</p>
+                <p className="lg:text-[16px] text-[12px] text-red-500">${(item.price-(item.price * item.discountPercentage) / 100).toFixed(2)}</p>
               </div>
               <div className="flex justify-between">
                 <p className="lg:text-[16px] text-[12px] capitalize">
                   {item.category ? item.category : "Others"}
                 </p>
-                <p className="lg:text-[12px] text-[9px] line-through">{item.price}</p>
+                <p className="lg:text-[12px] text-[9px] line-through">${item.price}</p>
               </div>
             </div>
           </SwiperSlide>
