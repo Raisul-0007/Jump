1a38d07a715b2f4011a2bf485fe137c3300284cb
Raisul-0007 <raisulislamriat007@gmail.com>
2026-07-18 07:23:33 +0600

    Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css'
 import Home from './pages/Home'
-
+import Layout from "./components/Layout"
+import Shop from "./pages/Shop"
 function App() {
+
+  let router = createBrowserRouter([
+    {
+      path:"/",
+      element: <Layout/>,
+      children:[
+    {
+      path:"/",
+      element:<Home/>,
+    },{
+      path:"/shop",
+      element:<Shop/>,
+    },
+  ],
+  },
+  ])
+
 
   return (
     <div className="">
-      <Home/>
+      <RouterProvider router={router}/>
     </div>
   )
 }
