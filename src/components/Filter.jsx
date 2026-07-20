1587aa36720a2398702aba6fd17bc6e2018b0e36
Raisul-0007 <raisulislamriat007@gmail.com>
2026-07-20 13:46:21 +0600

    category by price

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -11,7 +11,11 @@ const Filter = () => {
   },[info])
   const [activeCategory, setActiveCategory] = useState("");
   
-
+let [price, priceShow] = useState(false)
+  let [priceSet, priceSetShow] = useState([])
+  useEffect(()=>{
+    priceSetShow([...new Set(info.map((item)=> item.price))])
+  },[info])
 
   return (
     <div className="flex gap-5 ">
@@ -38,7 +42,26 @@ const Filter = () => {
           </ul>
           )}
         </div>
-      </div>  
+      </div> 
+      <div className="py-20">
+        <div onClick={(()=> priceShow(!price))} className="flex justify-between items-center border border-red-500 p-2">
+          <h3 className="text-3xl">Filter By Price</h3>
+          {price ? <FaChevronUp /> : <FaChevronDown />}
+        </div>
+        <div className={`overflow-hidden transition-all duration-500 ease-in-out ${
+    price
+      ? " opacity-100 mt-2"
+      : "max-h-0 opacity-0"
+  }`}>
+    {price &&(
+      <ul>
+        {priceSet.map((item)=>(
+          <li className="capitalize px-2 text-xl cursor-pointer transition-colors duration-300 border-b py-2">{item}</li>
+        ))}
+      </ul>
+    )}
+  </div>
+    </div>
     </div>
     <div className="w-3/4"></div>
     </div>
