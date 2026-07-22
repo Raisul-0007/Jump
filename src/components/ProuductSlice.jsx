82b753f3f574aaa545d4fe800a38b1a8eb933fc9
Raisul-0007 <raisulislamriat007@gmail.com>
2026-07-22 08:43:52 +0600

    product slice

--- src/components/ProuductSlice.jsx
+++ src/components/ProuductSlice.jsx
@@ -17,17 +17,48 @@ const ProuductSlice = ({filter}) => {
   useEffect(()=>{
     setCurrentPage(1)
   },[filter])
+
+  let pageNumber = []
+
+  for (let i = 1; i < (filter.length/perPage); i++) {
+    pageNumber.length[i]; 
+  }
+  let prev = ()=>{
+    if(currentPage > 1){
+      setCurrentPage((index)=> index - 1)
+    }
+  }
+  let paginate = (index)=>{
+    setCurrentPage(index + 1 )
+  }
+  let next = ()=>{
+    if(currentPage < currentPage.length){
+      setCurrentPage((index)=> index + 1)
+    }
+  }
+  let handlePageNumber = (e)=>{
+    setPerPage(e.target.value)
+    setCurrentPage(1)
+  }
   return (
     <div>
       <div className="flex justify-between">
         <div className="flex gap-2">
-        <div onClick={handleActive} className={`border border-red-500 p-2 cursor-pointer text-[12px]
+        <div onClick={handleActive} className={`border border-red-500 p-2 cursor-pointer 
           ${active === "active" ? "bg-red-500 hover:bg-red-800" : "hover:bg-red-500" }`}>
           <FaTableCellsLarge/>
         </div>
-        <div onClick={handleList} className={`border border-red-500 p-2 cursor-pointer text-[12px] ${active === "" ? "bg-red-500 hover:bg-red-800" : "hover:bg-red-500" }`}>
+        <div onClick={handleList} className={`border border-red-500 p-2 cursor-pointer  ${active === "" ? "bg-red-500 hover:bg-red-800" : "hover:bg-red-500" }`}>
           <FaListUl/>
         </div>
+      </div>
+      <div className="flex items-center gap-3">
+        <p className="">Show :</p>
+        <select onChange={handlePageNumber} name="" id="" className=" px-5 py-1  cursor-pointer border border-red-800 shadow-2xl">
+          <option value="12">12</option>
+          <option value="16">16</option>
+          <option value="20">20</option>
+        </select>
       </div>
       </div>
       <div className=""></div>
