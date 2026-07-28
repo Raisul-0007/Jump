e26eb37d33eef87db4a2fc91d22fe4a97ea41e8d
Raisul-0007 <raisulislamriat007@gmail.com>
2026-07-28 15:00:25 +0600

    Product Details start work

--- src/pages/PruductDetails.jsx
+++ src/pages/PruductDetails.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Container from '../components/Container'
-
+import useParams from 'react-router-dom'
+import axios from "axios"
 const PruductDetails = () => {
+  let productId = useParams()
+  let [product, setProduct] = useState()
+  useEffect(()=>{
+    axios.get(`https://dummyjson.com/products?&limit=0/${productId}`).then((res)=> setProduct(res.data))
+  },[productId])
   return (
     <div>
       <Container></Container>
