6940695ff098c16204edcb853dca28dd0ac0868e
Raisul-0007 <raisulislamriat007@gmail.com>
2026-07-30 14:34:12 +0600

    client rating

--- src/pages/PruductDetails.jsx
+++ src/pages/PruductDetails.jsx
@@ -2,12 +2,27 @@ import React, {useState, useEffect} from 'react'
 import Container from '../components/Container'
 import {useParams} from 'react-router-dom'
 import axios from "axios"
+import { TiStarHalf } from "react-icons/ti";
+import { IoIosStar, IoIosStarHalf, IoIosStarOutline } from "react-icons/io"
 const PruductDetails = () => {
   let {id} = useParams()
   let [product, setProduct] = useState()
   useEffect(()=>{
     axios.get(`https://dummyjson.com/products/${id}`).then((res)=> setProduct(res.data))
   },[id])
+
+  let clientRating = Array.from({length:5}, (_ , index)=>{
+    const  number = index + 0.5
+
+    return product?.rating > index + 1 ? (
+      <IoIosStar key={index}/>
+    ): product?.rating > number ? (
+      <TiStarHalf key={index}/>
+    ):(
+      <div className=""></div>
+    )
+  })
+
   return (
     <div>
       <Container>
@@ -18,7 +33,10 @@ const PruductDetails = () => {
           <div className="w-3/5">
             <div className="py-5">
               <p className="capitalize text-red-500">{product?.category}</p>
-              <h2 className="text-6xl py-5">{product?.title}</h2>
+              <h2 className="text-6xl py-2">{product?.title}</h2>
+            </div>
+            <div className="flex gap-0.5 text-amber-300">
+              {clientRating}
             </div>
           </div>
         </div>
