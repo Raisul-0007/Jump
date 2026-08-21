b701a1c4241454d6cd1231e0d1712f9589df20b0
Raisul-0007 <raisulislamriat007@gmail.com>
2026-08-21 11:13:53 +0600

    state set

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Container from '../components/Container'
 
 const Checkout = () => {
+  let [showPopUp, setShowPopUp] = useState(false)
+
+  let [name, setName] = useState("")
+  let [number, setNumber] = useState("")
+  let [email, setEmail] = useState("")
+  let [checked, setChecked] = useState(false)
+
+  let handleName = (e)=>{
+    setName(e.target.value)
+  }
+  let handleEmail = (e)=>{
+    setEmail(e.target.value)
+  }
+  let handlePayment = (e)=>{
+    e.preventDefault()
+    setName("")
+    setNumber("")
+    setEmail("")
+    setChecked(false)
+  }
+  let handlePopUp = ()=>{
+    window.location.reload()
+  }
   return (
     <div>
       <Container>
